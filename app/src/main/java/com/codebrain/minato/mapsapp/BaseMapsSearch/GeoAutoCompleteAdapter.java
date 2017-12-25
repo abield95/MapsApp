07d539006b983a6ff123f99f574c4d4f5d30a401
abield95 <santosabield55@gmail.com>
2017-12-25 12:56:13 -0600

    Adaptador para manejar las busquedas de los sitios

---
+++ app/src/main/java/com/codebrain/minato/mapsapp/BaseMapsSearch/GeoAutoCompleteAdapter.java
@@ -0,0 +1,12 @@
+package com.codebrain.minato.mapsapp.BaseMapsSearch;
+
+import android.widget.BaseAdapter;
+import android.widget.Filterable;
+
+/**
+ * Created by username on 12/25/2017.
+ */
+
+public class GeoAutoCompleteAdapter extends BaseAdapter implements Filterable{
+    
+}
