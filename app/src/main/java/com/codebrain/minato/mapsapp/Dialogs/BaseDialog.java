5e171c2f6065a19582f4e18df64d33ee5d51c237
abield95 <santosabield55@gmail.com>
2018-02-19 18:06:46 -0500

    code optimization

--- app/src/main/java/com/codebrain/minato/mapsapp/Dialogs/BaseDialog.java
+++ app/src/main/java/com/codebrain/minato/mapsapp/Dialogs/BaseDialog.java
@@ -1,22 +1,15 @@
 package com.codebrain.minato.mapsapp.Dialogs;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
-
-import com.codebrain.minato.mapsapp.R;
 
 /**
  * Created by username on 12/12/2017.
